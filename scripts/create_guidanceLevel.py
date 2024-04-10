cbdcb49743be57813e5e81a713be1c883fd108af
oiiio <gareth862@gmail.com>
2024-04-09 20:46:58 -0500

    adding guidanceLevel to all guidelines and an openai script used to do it

--- scripts/create_guidanceLevel.py
+++ scripts/create_guidanceLevel.py
@@ -6,6 +6,8 @@ Use chatGPT to take a recommendation descriptions and return only one of three g
 
 import openai
 import os
+import json
+import time
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -13,10 +15,56 @@ from openai import OpenAI
 client = OpenAI()
 client.api_key = os.getenv("OPENAI_API_KEY")
 
+files_completed = []
+#read file names to see which are done already
+with open("files_completed.txt", "r") as x:
+    for line in x:
+        files_completed.append(line.strip())
+
+
 
 # Open and loop through every JSON file in the /Users/gareth/PharmCAT_ddna/src/main/resources/org/pharmgkb/pharmcat/reporter/guidelines directory
 # For each file, loop through each object and for each recommendations element, send the element's ["text"]["html"] to the OpenAI API as the user message
+for filename in os.listdir("/Users/gareth/PharmCAT_ddna/src/main/resources/org/pharmgkb/pharmcat/reporter/guidelines"):
+    if filename.endswith(".json") and filename not in files_completed:
+        with open(f"/Users/gareth/PharmCAT_ddna/src/main/resources/org/pharmgkb/pharmcat/reporter/guidelines/{filename}") as f:
+            data = json.load(f)
+            print(f)
+            i = 0
+            for recommendation in data["recommendations"]:
+                completion = client.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                    {
+                        "role": "system", 
+                        "content": "You are a pharmacogenomic expert assigning one of three guidance levels to a recommendation description that will guide treatment. You will see a description of a pharmacogenomic guideline and need to assign a level to it. Your only output will be the level number (1, 2, or 3).\n\n\
+                            Level meanings:\n\n\
+                                1 - Use standard precautions. Either there is no recommendation given by the description or genotype shows no elevated risks or treatment-altering indicators for prescribing the medication according to standard practice.\n\
+                                2 - Should be used with monitoring. There are some optional suggestions provided to mitigate risk because the risk factor is moderate.\n\
+                                3 - Significant changes or risks. The recommendation indicates important changes in efficacy, toxicity, or other properties for the specified medication. The recommendation strongly indicates to avoid and use an alternate medication.\
+                                "     
+                        },
+                    {"role": "user", "content": recommendation["text"]["html"]}
+                    ]
+                )
+                print(recommendation["text"]["html"])
+                print(completion.choices[0].message.content)
+                data["recommendations"][i]["guidanceLevel"] = str(completion.choices[0].message.content)
+                i += 1
 
+                #sleep to allow for API rate limiting
+                time.sleep(1)
+        with open(f"/Users/gareth/PharmCAT_ddna/src/main/resources/org/pharmgkb/pharmcat/reporter/guidelines/{filename}", "w") as o:
+            json.dump(data, o, indent=4)
+        files_completed.append(filename)
+
+#write to file to keep track of which files have been completed
+with open("files_completed.txt", "w") as y:
+    for file in files_completed:
+        y.write(file + "\n")
+
+
+"""
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
   messages=[
@@ -34,3 +82,4 @@ completion = client.chat.completions.create(
 )
 
 print(completion.choices[0].message.content)
+"""
